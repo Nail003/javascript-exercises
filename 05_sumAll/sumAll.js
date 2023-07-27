6329c95d76f9efd6a33c49ef31130c8c73bffc27
Nail003 <danialzafarstudy003@gmail.com>
2023-07-27 09:55:32 +0500

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (
+        typeof start != "number" ||
+        typeof end != "number" ||
+        start < 0 ||
+        end < 0
+    )
+        return "ERROR";
 
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sum = 0;
+    for (let addNumberInSum = start; addNumberInSum <= end; addNumberInSum++) {
+        sum += addNumberInSum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
