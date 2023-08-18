4afc71d4b8c160264500ccd9dc3e9902fc3af38b
Nail003 <danialzafarstudy003@gmail.com>
2023-08-18 20:25:45 +0500

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (givenString) {
+    const trimmedString = givenString.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return trimmedString.split("").reverse().join("") === trimmedString;
 };
 
 // Do not edit below this line
