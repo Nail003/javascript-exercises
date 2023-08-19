2b469b71a035fd79a57ebe4bebc824cdf39890e7
Nail003 <danialzafarstudy003@gmail.com>
2023-08-19 12:28:03 +0500

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,18 @@
 const findTheOldest = function (peopleArray) {
     return peopleArray.reduce((oldestPerson, person) => {
-        if (!oldestPerson) return person;
-        if (oldestPerson.ageOfDeath < person.ageOfDeath) return person;
-        return oldestPerson;
+        const ageOfOldestPerson = getAge(oldestPerson);
+        const ageOfPerson = getAge(person);
+        return ageOfOldestPerson > ageOfPerson ? oldestPerson : person;
     }, {});
 };
+
+function getAge(person) {
+    let birth = person.yearOfBirth;
+    let death = person.yearOfDeath;
+    if (!birth) birth = new Date().getFullYear(); // If birth is undefined
+    if (!death) death = new Date().getFullYear(); // If date of death is undefined
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
