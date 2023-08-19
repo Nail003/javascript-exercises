5b7edbca4b05a97355b704b8195b1c268935565d
Nail003 <danialzafarstudy003@gmail.com>
2023-08-19 11:30:40 +0500

    Complete 10_fibonacci exercies

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    if (index < 0) return "OOPS";
+    if (index === 0) return 0;
 
+    prevValue = 0;
+    nextValue = 1;
+
+    for (i = 0; i < index - 1; i++) {
+        [prevValue, nextValue] = [nextValue, prevValue + nextValue]; // Array Destructuring
+    }
+
+    return nextValue;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (peopleArray) {
+    return peopleArray.reduce((oldestPerson, person) => {
+        if (!oldestPerson) return person;
+        if (oldestPerson.ageOfDeath < person.ageOfDeath) return person;
+        return oldestPerson;
+    }, {});
 };
 
 // Do not edit below this line
